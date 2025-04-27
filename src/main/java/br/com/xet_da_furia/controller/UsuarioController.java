03d12d9b268db9718150f83475dfc52e0c46f256
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-04-27 20:09:29 -0300

    Login e registro

--- src/main/java/br/com/xet_da_furia/controller/UsuarioController.java
+++ src/main/java/br/com/xet_da_furia/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package br.com.xet_da_furia.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.xet_da_furia.model.Usuario;
+import br.com.xet_da_furia.model.dto.LoginDto;
+import br.com.xet_da_furia.model.dto.RegisterDTO;
 import br.com.xet_da_furia.model.dto.UsuarioDetailsDTO;
 import br.com.xet_da_furia.model.dto.UsuarioResponseDTO;
+import br.com.xet_da_furia.service.AuthorizationService;
 import br.com.xet_da_furia.service.UsuarioService;
 import br.com.xet_da_furia.utils.ConversorDTO;
 import lombok.AllArgsConstructor;
@@ -23,6 +27,8 @@ public class UsuarioController {
 	
 	private UsuarioService usuarioService;
 	
+	private AuthorizationService authorizationService;
+	
 	@GetMapping
 	public List<UsuarioResponseDTO> buscarTodos() {		
 		return ConversorDTO.usuarios(usuarioService.findAll());
@@ -33,9 +39,14 @@ public class UsuarioController {
 		return usuarioService.buscarUsuario(id);
 	}
 	
-	@PostMapping
-	public UsuarioDetailsDTO criarUsuario(@RequestBody Usuario usuario) {
-		return usuarioService.save(usuario);
+	@PostMapping("/registrar")
+	public ResponseEntity<Object> criarUsuario(@RequestBody RegisterDTO registerDTO) {
+		return authorizationService.register(registerDTO);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<Object> login(@RequestBody LoginDto loginDto) {
+		return authorizationService.login(loginDto);
 	}
 }
 
