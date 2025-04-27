319a0697edc828d0056d6762227333f61b6bc9db
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-04-27 13:49:49 -0300

    Inicio do desafio

---
+++ src/main/java/br/com/xet_da_furia/XetDaFuriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.xet_da_furia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XetDaFuriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XetDaFuriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/xet_da_furia/XetDaFuriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xet_da_furia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XetDaFuriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
